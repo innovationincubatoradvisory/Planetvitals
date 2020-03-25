c66576f1423329620088e0c3cc5dddbef2a50789
NAEEM HADIQ <nhadiq@gmail.com>
2020-03-25 23:24:09 +0530

    added country count

--- js/Region-all.js
+++ js/Region-all.js
@@ -3,6 +3,7 @@ let areaChartValues = []
 let confirmedChartValues = []
 let diedChartValues = []
 let recoveredChartValues = []
+let affected= 0
 fetch('https://planetvitals-backend.iinerds.com/api/v1/covid-worldmeters/listAllMapData')
     .then(response => {
         return response.json()
@@ -17,6 +18,7 @@ fetch('https://planetvitals-backend.iinerds.com/api/v1/covid-worldmeters/listAll
                 diedChartValues.push(parseFloat(item.deaths))
                 recoveredChartValues.push(parseFloat(item.recovered))
                 let itemactive = item.confirmed - item.deaths - item.recovered;
+                affected= affected+1;
                 if (itemactive == 0) {
                     document.getElementById("tb-content").innerHTML += '<tr class="table-info">' + "<td>" + item.country + "</td>" + '<td>' + item.confirmed + "</td>" + '<td style="color:darkred;">' + item.deaths + "</td>" + '<td style="color:red;">' + item.serious_cases + "</td>" +'<td style="color:orange;">' + itemactive + "</td>" +'<td style="color:green;">'  + item.recovered + "</td>" +   "<td>" + item.totalcases_pp + "</td>" +"</tr>";
 
@@ -27,7 +29,9 @@ fetch('https://planetvitals-backend.iinerds.com/api/v1/covid-worldmeters/listAll
 
             });
         }
+        affected=affected-1;
         drawAreaChart(areaChartLabels.slice(0, 10), confirmedChartValues.slice(0, 10), diedChartValues.slice(0, 10), recoveredChartValues.slice(0, 10))
+        document.getElementById("count-country").innerHTML +=affected+" countries and territories";
     })
     .catch(err => {
         // Do something for an error here
