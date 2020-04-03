7f3e6cb1bc8a7d5d5652c9f24b066af1dcb19a01
Naeem Hadiq <nhadiq@gmail.com>
2020-04-03 20:03:50 +0530

    corrected issues in table

--- js/region-all-yesterday.js
+++ js/region-all-yesterday.js
@@ -9,11 +9,9 @@ fetch('https://planetvitals-backend-py.iinerds.com/prevday')
             regions.forEach(item => {
                 let itemactive = item.confirmed - item.deaths - item.recovered;
                 if (itemactive == 0) {
-                    document.getElementById("tb-content-2").innerHTML += '<tr class="table-info">' + "<td>" + item.country + "</td>" + '<td>' + item.confirmed + "</td>" + '<td>' + item.change_confirmed + "</td>" + '<td style="color:darkred;">' + item.deaths + "</td>" + '<td style="color:darkred;">' + item.change_deaths + "</td>" +  '<td style="color:red;">' + item.serious_cases + "</td>" +'<td style="color:orange;">' + itemactive + "</td>" +'<td style="color:green;">'  + item.recovered + "</td>" +   "<td>" + item.totalcases_pp + "</td>" +"<td>" + item.deaths_pp + "</td>" +"<td>" + item.first_case + "</td>" +"</tr>";
-                    console.log("item.country")
+                    document.getElementById("tb-content-2").innerHTML += '<tr class="table-info">' + "<td>" + item.country + "</td>" + '<td>' + item.confirmed + "</td>" + '<td>' + item.change_confirmed + "</td>" + '<td style="color:darkred;">' + item.deaths + "</td>" + '<td style="color:darkred;">' + item.change_deaths + "</td>" +  '<td style="color:red;">' + item.serious_cases + "</td>" +'<td style="color:orange;">' + itemactive + "</td>" +'<td style="color:green;">'  + item.recovered + "</td>" +   "<td>" + item.totalcases_pp + "</td>" +"<td>" + item.deaths_pp + "</td>" +"</tr>";
                 } else {
-                    document.getElementById("tb-content-2").innerHTML += "<tr>"  + "<td>" + item.country + "</td>" + '<td>' + item.confirmed + "</td>" + '<td>' + item.change_confirmed + "</td>" + '<td style="color:darkred;">' + item.deaths + "</td>" + '<td style="color:darkred;">' + item.change_deaths + "</td>" +  '<td style="color:red;">' + item.serious_cases + "</td>" +'<td style="color:orange;">' + itemactive + "</td>" +'<td style="color:green;">'  + item.recovered + "</td>" +   "<td>" + item.totalcases_pp + "</td>" +"<td>" + item.deaths_pp + "</td>" +"<td>" + item.first_case + "</td>" +"</tr>";
-                    console.log("item.country")
+                    document.getElementById("tb-content-2").innerHTML += "<tr>"  + "<td>" + item.country + "</td>" + '<td>' + item.confirmed + "</td>" + '<td>' + item.change_confirmed + "</td>" + '<td style="color:darkred;">' + item.deaths + "</td>" + '<td style="color:darkred;">' + item.change_deaths + "</td>" +  '<td style="color:red;">' + item.serious_cases + "</td>" +'<td style="color:orange;">' + itemactive + "</td>" +'<td style="color:green;">'  + item.recovered + "</td>" +   "<td>" + item.totalcases_pp + "</td>" +"<td>" + item.deaths_pp + "</td>"  +"</tr>";
                 }
 
             });
