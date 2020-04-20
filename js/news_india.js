d02d53e3963494e0b9244aa3ae70d4c309e7f17e
tkpranav <47771280+tkpranav@users.noreply.github.com>
2020-04-21 04:49:17 +0530

    added kerala.html

---
+++ js/news_india.js
@@ -0,0 +1,28 @@
+let dt = '<div class="col-lg-4 col-md-6 content-item" data-aos="fade-up">';
+fetch(
+  "https://newsapi.org/v2/top-headlines?q=corona&language=en&apiKey=5b67480ba15d44889bbfcbf108d596ef"
+)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    articles = data.articles;
+    if (articles && articles.length) {
+      articles.forEach((item) => {
+        document.getElementById("news-row").innerHTML +=
+          dt +
+          '<span><img src="' +
+          item.urlToImage +
+          '"width="80%"><h4>' +
+          item.title +
+          "</h4><p>" +
+          item.description +
+          '<a href="' +
+          item.url +
+          '">Read More</a></p></div>';
+      });
+    }
+  })
+  .catch((err) => {
+    // Do something for an error here
+  });
